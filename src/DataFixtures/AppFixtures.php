812e5929739ac9c98be8c570193be776836340ca
Emilien Mattei <emilien_77400@hotmail.fr>
2023-11-29 17:31:58 +0100

    choix + palier

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Choix;
+use App\Entity\Scenario;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,7 +13,18 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+        $choix = new Choix();
+        $choix->setNom('test choix');
+        $manager->persist($choix);
 
+        $choix->setDescription('testtestestes');
+
+        $scenario = new Scenario();
+        $scenario->setNom('test');
+        $scenario->addIdChoix($choix);
+        $scenario->setPalier(1);
+        
+        $manager->persist($scenario);
         $manager->flush();
     }
 }
