5b69ec85c9c655902e9999d6c35fb970dd22383f
Emilien Mattei <emilien_77400@hotmail.fr>
2023-11-29 16:03:20 +0100

    contraintes points

--- src/Form/LicorneType.php
+++ src/Form/LicorneType.php
@@ -6,7 +6,7 @@ use App\Entity\Licorne;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints\Callback;
 
 
 class LicorneType extends AbstractType
@@ -25,6 +25,24 @@ class LicorneType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Licorne::class,
+            'constraints' =>[
+                new Callback([$this,'validSum'])
+            ]
         ]);
+    }
+
+    public function validSum($data,$context){
+        $total = $data->getStrenght() + $data->getIntelligence() + $data->getEsquive();
+        if($total !== 5){
+            $context->buildViolation('Le total des points doit être de 5')
+                ->atPath('strenght')
+                ->addViolation();
+            $context->buildViolation('Le total des points doit être de 5')
+                ->atPath('intelligence')
+                ->addViolation();
+            $context->buildViolation('Le total des points doit être de 5')
+                ->atPath('esquive')
+                ->addViolation();
+        }
     }
 }
